cf3209f1151b18f1c46ac9dec19882154ebec327
rashmikanaveen <rashmikanaveen734@gmail.com>
2025-01-11 01:56:33 +0530

    feat: refactor cartReducer to use localStorage for cartItems and clean up code

--- frontend/src/reducers/cartReducer.jsx
+++ frontend/src/reducers/cartReducer.jsx
@@ -3,44 +3,66 @@ import React, { createContext, useReducer } from 'react';
 
 const CartContext = createContext();
 
-const initialState = {
-  cartItems: JSON.parse(Cookies.get("cartItems") || "[]"),
-};
-
-export const cartReducer = (state = initialState, action) => {
-  let updatedCartItems=[];
-  switch (action.type) {
-    case "ADD_TO_CART":
-      const item = action.payload;
-      const existItem = state.cartItems.find(
-        (x) => x._id === item._id && x.varient === item.varient && item.quantity === item.quantity
-      );
-      if (existItem) {
-        updatedCartItems = state.cartItems.map((x) =>
-          x._id === existItem._id && x.varient === existItem.varient ? item : x
-        );
-
-      }else{
-         updatedCartItems = [...state.cartItems, item];
-      }
-      
-        Cookies.set("cartItems", JSON.stringify(updatedCartItems));
-        console.log("Updated Cart Items:", updatedCartItems);
-        return {
-          ...state,
-          cartItems: updatedCartItems,
-        };
-
-    default:
-      return state;
-  }
-};
 
 
 
 
-export const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use when need to use cookies
+
+//
+//const initialState = {
+//  cartItems: JSON.parse(Cookies.get("cartItems") || "[]"),
+//};
+//
+//export const cartReducer = (state = initialState, action) => {
+//  let updatedCartItems=[];
+//  switch (action.type) {
+//    case "ADD_TO_CART":
+//      const item = action.payload;
+//      const existItem = state.cartItems.find(
+//        (x) => x._id === item._id && x.varient === item.varient && item.quantity === item.quantity
+//      );
+//      if (existItem) {
+//        updatedCartItems = state.cartItems.map((x) =>
+//          x._id === existItem._id && x.varient === existItem.varient ? item : x
+//        );
+//
+//      }else{
+//         updatedCartItems = [...state.cartItems, item];
+//      }
+//      
+//        Cookies.set("cartItems", JSON.stringify(updatedCartItems));
+//        console.log("Updated Cart Items:", updatedCartItems);
+//        return {
+//          ...state,
+//          cartItems: updatedCartItems,
+//        };
+//
+//    default:
+//      return state;
+//  }
+//};
+//
+//
+
+
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, {
+    cartItems: JSON.parse(localStorage.getItem('cartItems') || '[]'),
+  });
 
   return (
     <CartContext.Provider value={{ state, dispatch }}>
@@ -49,4 +71,38 @@ export const CartProvider = ({ children }) => {
   );
 };
 
+export { CartContext, CartProvider };
 
+
+
+
+const initialState = {
+  cartItems: JSON.parse(localStorage.getItem('cartItems') || '[]'),
+};
+
+export const cartReducer = (state = initialState, action) => {
+  let updatedCartItems = [];
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      const item = action.payload;
+      const existItem = state.cartItems.find(
+        (x) => x._id === item._id && x.varient === item.varient
+      );
+
+      if (existItem) {
+        updatedCartItems = state.cartItems.map((x) =>
+          x._id === existItem._id && x.varient === existItem.varient ? item : x
+        );
+      } else {
+        updatedCartItems = [...state.cartItems, item];
+      }
+
+      localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+      return {
+        ...state,
+        cartItems: updatedCartItems,
+      };
+    default:
+      return state;
+  }
+};
